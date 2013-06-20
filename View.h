6c7bdc276ee967e28d906a68d7e0a32ec0a1daae
illdoit <andrewehigiator@gmail.com>
2013-06-20 13:30:31 -0400

    Revert "Revert "Jun20""
    This reverts commit 256984e01ff84d615f010ccc730e54caa5b7baf0.

---
+++ View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  Jun20
+//
+//  Created by Sir Andrew on 6/20/13.
+//  Copyright (c) 2013 edu.nyu.spcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View: UIView
+
+@end
